904aecbdd691f81e358efca1e3cae85fb032e2f4
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-16 01:16:14 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -2,6 +2,6 @@ var recipes={}
 
 function updateObjectWithKeyAndValue(object, key, value){
 var tempObj={key, value};
-  var newObj =  Object.assign({}, object, tempObj
+  var newObj =  Object.assign({}, object, tempObj)
   return newObj;
 }
