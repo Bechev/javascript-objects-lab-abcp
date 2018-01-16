384cbbd8914bb6d2a4499b5f4e296f11b5b9dd75
Bertrand Chevalier <bertrand.chevalier@gmail.com>
2018-01-16 01:32:24 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 var recipes={}
 
 function updateObjectWithKeyAndValue(object, key, value){
-  var tempObj= {[key]: value}
-  var newObj =  Object.assign({}, object, tempObj)
+  var tempObj= {[key]: value};
+  var newObj =  Object.assign({}, object, tempObj);
   return newObj;
 }
 
@@ -12,5 +12,6 @@ function destructivelyUpdateObjectWithKeyAndValue(object,key,value){
 }
 
 function deleteFromObjectByKey(object,key){
-  var newObj = delete object[key]
+  var newObj = Object.assign({},object);
+  delete newObj[key];
 }
